54ea20d34e8c30e71f88199f1fe3d96e79b012fe
annakreno <annakreno@gmail.com>
2023-09-15 17:48:51 -0500

    fix mobile styling bug, adding hamburger menu for small screens

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom';
 import { useLocation } from "react-router-dom";
+import React, { useState } from 'react';
 import logo from '../../images/logo.png';
 import name from '../../images/name.png';
 import "./NavBar.css"
 
 export default function NavBar() {
   const location = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false)
+  }
+
   const activeLinkClass = function(path) {
     if (location.pathname === path) { 
         return 'active-nav-links'
@@ -21,26 +32,30 @@ export default function NavBar() {
     }
 };
     return (
-      <div className="nav-container">
+      <div className={`nav-container ${menuOpen ? 'menu-open' : 'menu-close'}`}>
+        <div className="menu-button" onClick={toggleMenu}>
+          <div className={`menu-bar ${menuOpen ? 'open' : ''}`} />
+          <div className={`menu-bar ${menuOpen ? 'open' : ''}`} />
+          <div className={`menu-bar ${menuOpen ? 'open' : ''}`} />
+        </div>
         <div className='nav-links' id="navLogo">
-          <Link to="/"><img src={name} /></Link>
+          <Link to="/" onClick={closeMenu}><img src={name} /></Link>
         </div>
         <div className={activeLinkClass('/about')}>
-          <Link to="/about">about</Link>
+          <Link to="/about" onClick={closeMenu}>about</Link>
         </div>
         {/* <div className={activeLinkClass('/credits')}>
-          <Link to="/credits">credits</Link>
+          <Link to="/credits" onClick={closeMenu}>credits</Link>
         </div> */}
         <div className={activeLinkClass('/equipment')}>
-          <Link to="/equipment">equipment</Link>
+          <Link to="/equipment" onClick={closeMenu}>equipment</Link>
         </div>
         <div className={activeLinkClass('/work')}>
-          <Link to="/work">work</Link>
+          <Link to="/work" onClick={closeMenu}>work</Link>
         </div>
         <div className={activeLinkClass('/contact')}>
-          <Link to="/contact">contact</Link>
+          <Link to="/contact" onClick={closeMenu}>contact</Link>
         </div>
-        
       </div>
     );
   }
